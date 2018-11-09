Fix gradient, change newton to gradient, create genframework

RaykarDS/em_raykarDS.py
@@ -11,6 +11,7 @@ from functools import partial
 
 EPS = 1e-8
 
+
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
@@ -18,6 +19,12 @@ def sigmoid(x):
 def newton(w, grad_func, hess_func, step_counts=100, step=0.01):
     for i in range(step_counts):
         w -= step * np.matmul(np.linalg.inv(hess_func(w)), grad_func(w))
+    return w
+
+
+def grad_descent(w, grad_func, step_counts=100, step=0.001):
+    for i in range(step_counts):
+        w += step * grad_func(w)
     return w
 
 
@@ -72,24 +79,21 @@ class EM_DS_Raykar:
         :param mu:
         :return:
         """
-        return ((mu - sigmoid(np.matmul(self.x, w)))*np.transpose(self.x)).sum(axis=1)
+        p = self.p_y1(w)
+        koeff = p*(1 - p)*(mu*self.l/(p*self.l + 1 - self.l) - (1 - mu)*self.l/((1 - p)*self.l + 1 - self.l))
+        return np.squeeze(np.matmul(koeff[None, :], self.x))
 
-    def hess_w(self, w):
-        """
-        Hessian of w.
-        :param w: Weights in linear regression.
-        :return:
-        """
-        trans_x = np.transpose(self.x)
-        x_with_values = np.transpose((1 - sigmoid(np.matmul(self.x, w)))*sigmoid(np.matmul(self.x, w))*trans_x)
-        ans = -np.dot(trans_x[None, :, :], x_with_values[None, :, :])
-        ans_sq = np.squeeze(ans)
-        return ans_sq
+    # def hess_w(self, w):
+    #     """
+    #     Hessian of w.
+    #     :param w: Weights in linear regression.
+    #     :return:
+    #     """
 
     def update_vars(self, w, mu):
         new_alpha = np.matmul(np.transpose(self.y), mu)/(mu.sum())
         new_beta = np.matmul((1 - np.transpose(self.y)), (1 - mu))/((1 - mu).sum())
-        new_w = newton(w, partial(self.grad_w, mu=mu), self.hess_w, 100)
+        new_w = grad_descent(w, partial(self.grad_w, mu=mu), 100)
         return new_alpha, new_beta, new_w
 
     def e_loglikelihood(self, alpha, beta, w, mu):
@@ -131,9 +135,12 @@ class EM_DS_Raykar:
             if self.verbose:
                 self.out(step, alpha, beta, w, mu, exp_new)
 
+            if step > 10000:
+                break
+
         return alpha, beta, w, mu
 
     @staticmethod
     def out(step, alpha, beta, w, mu, exp_new):
-        print("--------------------\nStep={}\nalpha={}\nbeta={}\nw={}\nmu={}\nlog E={}\n"
-              .format(step, alpha, beta, w, mu, exp_new))
+        print("--------------------\nStep={}\nlog E={}\n"
+              .format(step, exp_new))

RaykarDS/generate_data.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 import pandas as pd
-from em import sigmoid
+from em_raykarDS import sigmoid
 
 
 def generate_A_points(n, m, d,
@@ -76,45 +76,29 @@ def generate_points(n, m, d, l):
     print('w={}'.format(w))
 
     x = np.random.normal(size=(n, d))
-    y_workers = np.zeros((n*m, 4))
+    y_all = np.zeros((n*m, 4))
 
     model_true = np.random.binomial(size=n, n=1, p=l)
     model_answer = np.where(model_true, (np.squeeze(sigmoid(np.matmul(x, w[:, None]))) > 0.5).astype(int),
                             (np.squeeze(sigmoid(np.matmul(x, w[:, None]))) < 0.5).astype(int))
-    workers_true = np.random.binomial(size=(n, m), n=1, p=sigmoid(np.matmul(x, np.transpose(u))))
+    workers_labels = np.random.binomial(size=(n, m), n=1, p=sigmoid(np.matmul(x, np.transpose(u))))
 
-    workers_labels = np.transpose(np.where(np.logical_xor(model_true, np.transpose(workers_true)),
-                              1 - model_answer, model_answer))
-    y_workers[:, 2] = workers_labels.flatten()
+    y_all[:, 2] = workers_labels.flatten()
 
     y_real = np.where(model_true, model_answer, 1 - model_answer)
     for i in range(n):
-        y_workers[i*m:(i + 1)*m, 0] = i
-        y_workers[i*m:(i + 1)*m, 1] = list(range(m))
-        y_workers[i*m:(i + 1)*m, 3] = y_real[i]
+        y_all[i*m:(i + 1)*m, 0] = i
+        y_all[i*m:(i + 1)*m, 1] = list(range(m))
+        y_all[i*m:(i + 1)*m, 3] = y_real[i]
 
-    return x, y_real, y_workers
+    return x, y_real, workers_labels, y_all, w
 
 
-def save_points(x, y_real, y_workers, crowd_file, task_file,
+def create_dfs(x, y_real, y_workers,
                 worker_column='worker_id', task_column='task_id', worker_ans_column='response', gold_ans_column='gold'):
     task_pd = pd.DataFrame(np.hstack(
         (np.array(list(range(x.shape[0]))).reshape((x.shape[0], 1)), y_real.reshape((x.shape[0], 1)), x)),
                              columns=[task_column] + [gold_ans_column] + ['{}_feature'.format(i) for i in range(x.shape[1])])
     crowds_pd = pd.DataFrame(y_workers, columns=[task_column, worker_column, worker_ans_column, gold_ans_column])
 
-    task_pd.to_csv(task_file, sep='\t', index=False)
-    crowds_pd.to_csv(crowd_file, sep='\t', index=False)
-
-if __name__ == '__main__':
-    # x, y_real, y_workers = generate_A_points(100, 10, 3, 0.3, 0.6, np.array([1, 2, 3]), 0.55)
-    # save_points(x, y_real, y_workers, 'generated/crowd_A.tsv', 'generated/tasks_A.tsv')
-    #
-    # x, y_real, y_workers = generate_B_points(100, 10, 3, 0.3, 0.6, np.array([1, 2, 3]), 0.55)
-    # save_points(x, y_real, y_workers, 'generated/crowd_B.tsv', 'generated/tasks_B.tsv')
-    #
-    # x, y_real, y_workers = generate_AB_points(100, 10, 3, 0.3, 0.6, np.array([1, 2, 3]), 0.55)
-    # save_points(x, y_real, y_workers, 'generated/crowd_AB.tsv', 'generated/tasks_AB.tsv')
-    #
-    x, y_real, y_workers = generate_points(2, 4, 3, 0.7)
-    save_points(x, y_real, y_workers, 'generated/crowd.tsv', 'generated/tasks.tsv')
+    return task_pd, crowds_pd

RaykarDS/run_DS_raykar.py
@@ -2,7 +2,7 @@ import argparse
 import pandas as pd
 import numpy as np
 
-from em import EM_DS_Raykar
+from em_raykarDS import EM_DS_Raykar
 
 
 def run_ds_reykar(crowd_data, tasks_data, worker_column, task_column, worker_ans_column, gold_ans_column):
@@ -28,14 +28,15 @@ def run_ds_reykar(crowd_data, tasks_data, worker_column, task_column, worker_ans
     print('alpha={}'.format(alpha))
     print('beta={}'.format(beta))
     print('w={}'.format(w))
+    print('grad w={}'.format(em_ds_raykar.grad_w(w, mu)))
     print("P real = {}".format(np.where(y_real == 1, mu, 1 - mu)))
     print('Elog={}'.format(em_ds_raykar.e_loglikelihood(alpha, beta, w, mu)))
     print("Overall error: {}".format((abs(y_real - mu)).mean()))
 
     real_mu = y_real
-    # real_w = np.array([0.4105985, 0.14404357, 1.45427351])
-    # real_w = np.array([0.15494743, 0.37816252, -0.88778575])
     real_w = np.array([0.76103773, 0.12167502, 0.44386323])
+    # real_w = np.array([0.15494743, 0.37816252, -0.88778575])
+    # real_w = np.array([0.76103773, 0.12167502, 0.44386323])
 
     alpha, beta, w = em_ds_raykar.update_vars(real_w, real_mu)
     print('Ereallog={}'.format(em_ds_raykar.e_loglikelihood(alpha, beta, real_w, real_mu)))

RaykarDS/tests/test_raykarDS_calc.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from em import EM_DS_Raykar, sigmoid
+from em_raykarDS import EM_DS_Raykar, sigmoid
 
 
 class TestCalculations(unittest.TestCase):
@@ -36,30 +36,19 @@ class TestCalculations(unittest.TestCase):
 
     def test_grad_w(self):
         x = np.array([[1, 2, 1], [0, 2, 2]])
+        l = 0.6
 
-        em_dsraykar = EM_DS_Raykar(y=None, x=x, l=None)
+        em_dsraykar = EM_DS_Raykar(y=None, x=x, l=l)
 
         mu = np.array([0.3, 0.2])
         w = np.array([1, 2, 3])
 
         ans = np.zeros((x.shape[1],))
         for i in range(x.shape[0]):
-            ans += x[i]*(mu[i] - sigmoid(np.sum(w*x[i])))
+            cur_p = sigmoid(np.sum(w*x[i]))
+            ans += x[i]*cur_p*(1 - cur_p)*(mu[i]*(l/(cur_p*l + (1 - l))) + (1 - mu[i])*(-l/((1 - cur_p)*l + 1 - l)))
 
         self.assertTrue(np.allclose(ans, em_dsraykar.grad_w(w, mu)))
-
-    def test_hess_w(self):
-        x = np.array([[1, 2, 1], [0, 2, 2]])
-
-        em_dsraykar = EM_DS_Raykar(y=None, x=x, l=None)
-
-        w = np.array([1, 2, 3])
-
-        ans = np.zeros((x.shape[1], x.shape[1]))
-        for i in range(x.shape[0]):
-            ans -= np.matmul(np.transpose(x[i, None]), x[i, None])*(1 - sigmoid(np.sum(w*x[i])))*sigmoid(np.sum(w*x[i]))
-
-        self.assertTrue(np.allclose(ans, em_dsraykar.hess_w(w)))
 
     def test_e_loglikelihood(self):
         x = np.array([[1, 2], [2, 3], [0, 1]])